23ffa78dcf80b5c10d93dd80650bea37b9bb4508
김용범 <subum12345@gmail.com>
2022-07-26 00:30:08 +0900

    mongoDB 설정

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,22 @@
+// npm init
+// npm install express --save
+// npm install mongoose --save
+
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://yungbum42:gkdks120@boilerplate.cyqvai5.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
