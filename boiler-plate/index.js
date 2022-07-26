66cf57e7c34abe0b7ceece7e1969406ba3234b54
김용범 <subum12345@gmail.com>
2022-07-26 14:38:01 +0900

    node express 회원가입

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,10 +1,16 @@
 // npm init
 // npm install express --save
 // npm install mongoose --save
+// npm install body-parser --save
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -19,4 +25,21 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  // bodyParser 덕분에 바로 req.body사용가능
+  const user = new User(req.body);
+
+  // mongoDB에 저장
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
