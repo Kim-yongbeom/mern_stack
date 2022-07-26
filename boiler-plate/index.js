d96b86ad6c4b3f3c1e9cb7b40a1503e4953e9c56
김용범 <subum12345@gmail.com>
2022-07-26 22:00:57 +0900

    Auth기능 만들기

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const mongoose = require("mongoose");
+const { auth } = require("./middleware/auth");
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -32,7 +33,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!!"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
 
@@ -48,7 +49,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -77,6 +78,23 @@ app.post("/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+// 권한기능 생성
+// auth 미들웨어를 추가해줌
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 뜻
+  res.status(200).json({
+    _id: req.user._id,
+    // role 이 0이면 일반유저 0이 아니면 관리자
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
