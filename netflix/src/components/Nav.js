7a8343be0f3b1d60430fd9461ee7fb04ccf24596
김용범 <subum12345@gmail.com>
2022-08-15 18:42:49 +0900

    netflix 꾸미기

--- netflix/src/components/Nav.js
+++ netflix/src/components/Nav.js
@@ -1,9 +1,21 @@
 import React from "react";
+import netflix from "../pngwing.png";
+import "./Nav.css";
 
 function Nav() {
   return (
-    <nav>
-      <img alt="Netflix logo" />
+    <nav className="nav">
+      <img
+        alt="Netflix logo"
+        src={netflix}
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
+        className="nav__avatar"
+      />
     </nav>
   );
 }
