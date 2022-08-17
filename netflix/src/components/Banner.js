a09a647715d6ed481afa4f8e6646e689809d1551
김용범 <subum12345@gmail.com>
2022-08-17 22:05:14 +0900

    netflix banner

--- netflix/src/components/Banner.js
+++ netflix/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
 import requests from "../api/requests";
+import "./Banner.css";
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
@@ -26,5 +27,30 @@ export default function Banner() {
     setMovie(movieDetail);
   };
 
-  return <div></div>;
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button info">More Information</button>
+        </div>
+        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 }
